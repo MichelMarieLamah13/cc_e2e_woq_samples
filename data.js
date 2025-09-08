ef7a89bf7d93e37ad2d2cb86ce2098ac77792f49
lamm3254 <lamahmichelmarie@gmail.com>
2025-09-07 20:30:15 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,277 @@
+export const DATA = [
+    {
+        title: "Feature size variation",
+        hide: false,
+        description: [],
+        content: [
+            {
+                title: "<i class='fas fa-male'></i> Male 1",
+                transcription: "Slide the box into that empty space. The plant grew large and green in the window.",
+                hide: false,
+                content: [
+                    {
+                        title: "Original input",
+                        path: "WB_samples/original/M1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "7.2 kbps (original)",
+                        path: "WB_samples/CC_7200/M1_cc_7200_orig.flac",
+                        hide: false
+                    },
+
+                ]
+            },
+            {
+                title: "",
+                hide: false,
+                content: [
+                    {
+                        title: "8 features",
+                        path: "WB_samples/original/M1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "16 features",
+                        path: "WB_samples/original/M1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "32 features",
+                        path: "WB_samples/original/M1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "64 features",
+                        path: "WB_samples/original/M1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "128 features",
+                        path: "WB_samples/original/M1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "256 features",
+                        path: "WB_samples/original/M1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "512 features",
+                        path: "WB_samples/original/M1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "1024 features",
+                        path: "WB_samples/original/M1_orig.flac",
+                        hide: false
+                    },
+
+                ]
+            },
+            {
+                title: "<i class='fas fa-male'></i> Male 2",
+                transcription: "He broke a new shoelace that day. The coffee stand is too high for the couch.",
+                hide: false,
+                content: [
+                    {
+                        title: "Original input",
+                        path: "WB_samples/original/M2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "7.2 kbps (original)",
+                        path: "WB_samples/CC_7200/M2_cc_7200_orig.flac",
+                        hide: false
+                    },
+
+                ]
+            },
+            {
+                title: "",
+                hide: false,
+                content: [
+                    {
+                        title: "8 features",
+                        path: "WB_samples/original/M2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "16 features",
+                        path: "WB_samples/original/M2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "32 features",
+                        path: "WB_samples/original/M2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "64 features",
+                        path: "WB_samples/original/M2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "128 features",
+                        path: "WB_samples/original/M2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "256 features",
+                        path: "WB_samples/original/M2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "512 features",
+                        path: "WB_samples/original/M2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "1024 features",
+                        path: "WB_samples/original/M2_orig.flac",
+                        hide: false
+                    },
+
+                ]
+            },
+            {
+                title: "<i class='fas fa-female'></i> Female 1",
+                transcription: "The idea is to show both are just straight. The kitten chased the dog down the street.",
+                hide: false,
+                content: [
+                    {
+                        title: "Original input",
+                        path: "WB_samples/original/F1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "7.2 kbps (original)",
+                        path: "WB_samples/CC_7200/F1_cc_7200_orig.flac",
+                        hide: false
+                    },
+
+                ]
+            },
+            {
+                title: "",
+                hide: false,
+                content: [
+                    {
+                        title: "8 features",
+                        path: "WB_samples/original/F1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "16 features",
+                        path: "WB_samples/original/F1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "32 features",
+                        path: "WB_samples/original/F1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "64 features",
+                        path: "WB_samples/original/F1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "128 features",
+                        path: "WB_samples/original/F1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "256 features",
+                        path: "WB_samples/original/F1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "512 features",
+                        path: "WB_samples/original/F1_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "1024 features",
+                        path: "WB_samples/original/F1_orig.flac",
+                        hide: false
+                    },
+
+                ]
+            },
+            {
+                title: "<i class='fas fa-female'></i> Female 2",
+                transcription: "The stray cat gave birth to kittens. The young girl gave no clear response.",
+                hide: false,
+                content: [
+                    {
+                        title: "Original input",
+                        path: "WB_samples/original/F2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "7.2 kbps (original)",
+                        path: "WB_samples/CC_7200/F2_cc_7200_orig.flac",
+                        hide: false
+                    },
+
+                ]
+            },
+            {
+                title: "",
+                hide: false,
+                content: [
+                    {
+                        title: "8 features",
+                        path: "WB_samples/original/F2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "16 features",
+                        path: "WB_samples/original/F2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "32 features",
+                        path: "WB_samples/original/F2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "64 features",
+                        path: "WB_samples/original/F2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "128 features",
+                        path: "WB_samples/original/F2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "256 features",
+                        path: "WB_samples/original/F2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "512 features",
+                        path: "WB_samples/original/F2_orig.flac",
+                        hide: false
+                    },
+                    {
+                        title: "1024 features",
+                        path: "WB_samples/original/F2_orig.flac",
+                        hide: false
+                    },
+
+                ]
+            }
+        ]
+    },
+]
+
+export const NOTE = [
+    "In these experiments, we trained the <b>cognitive coding codec</b> without a quantizer, following an <b>end-to-end</b> training approach.",
+    "This training strategy was adopted to estimate the maximum quality the model could potentially achieve.",
+    "We trained the models on a reduced, balanced subset of the <b>LibriSpeech-100</b> dataset, with each training taking approximately <b>1 day</b>.",
+    "We experimented with feature dimensions ranging from <b>8</b> to <b>1024</b>, increasing by a factor of <b>2</b> at each step.",
+    "The evaluation samples consist of <b>4</b> audio files, including recordings of both <b>male</b> and <b>female</b> speakers."
+]
---
+++ index.js
@@ -0,0 +1,126 @@
+import { DATA, NOTE } from "./data.js";
+
+
+function add_note() {
+    const note = document.querySelector("#section-0 .note")
+    note.innerHTML = ""
+    NOTE.forEach(content => {
+        const li = document.createElement("li")
+        li.innerHTML = content
+        note.appendChild(li)
+    })
+
+}
+
+function add_toc(parent, id, title) {
+    const li = document.createElement("li")
+    li.innerHTML = `<li><a href='#section-${id}'>${title}</a></li>`
+    parent.appendChild(li)
+}
+
+function add_section(data) {
+    const section = document.createElement("div")
+    section.id = "section-" + data.id
+    section.className = "section"
+
+    const previous = document.getElementById("section-" + (data.id - 1))
+
+    const h2 = document.createElement("h2")
+    h2.innerHTML = data.title
+    section.appendChild(h2)
+    
+    if (data.description){
+        const DESC  = document.createElement("ul");
+        data.description.forEach(content => {
+            const li = document.createElement("li")
+            li.innerHTML = content
+            DESC.appendChild(li)
+        })
+        section.appendChild(DESC)
+    }
+    
+
+    data.content.forEach(d => {
+        if (!d.hide) {
+            const h3 = document.createElement("h3")
+            h3.innerHTML = d.title
+            section.appendChild(h3)
+            if (d.transcription){
+                const p = document.createElement("p")
+                p.innerHTML = `
+                        <p> <b>Transcription</b>:  ${d.transcription}</p>
+                    `
+                section.appendChild(p)
+            }
+            const divs = document.createElement("div")
+            divs.className = "playlist"
+            d.content.forEach(c => {
+                if (!c.hide) {
+                    const div = document.createElement("div")
+                    div.className = "labeled-audio labeled-audio2"
+                    div.innerHTML = `
+                        <p class='text text2'> ${c.title}</p>
+                        <audio preload='metadata' controls><source class='select-real' src='${c.path}' type='audio/mpeg'></audio>
+                    `
+                    divs.appendChild(div)
+                }
+            })
+            section.appendChild(divs)
+        }
+    })
+
+
+    const hr = document.createElement("hr")
+
+    previous.after(hr)
+    hr.after(section)
+}
+
+function clear_sections() {
+    const targetSection = document.getElementById("section-0");
+    
+    if (targetSection) {
+        let current = targetSection.nextElementSibling;
+
+        while (current) {
+            const next = current.nextElementSibling;
+
+            if (current.id && current.id.toLowerCase().includes("section")) {
+                current.remove()
+            }
+
+            current = next;
+        }
+    }
+}
+function create_content() {
+    add_note()
+    const toc = document.querySelector("#section-0 .toc")
+    toc.innerHTML = ""
+
+    const hrs = document.querySelectorAll("hr")
+    hrs.forEach(hr => {
+        hr.remove()
+    })
+
+    const divs = document.querySelectorAll(".labeled-audio")
+    divs.forEach(div => {
+        div.remove()
+    })
+
+    clear_sections()
+
+    DATA.forEach((d, pos) => {
+        d.id = pos+1
+        if (!d.hide) {
+            add_toc(toc, d.id, d.title)
+            add_section(d)
+        }
+    })
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    create_content()
+});
+
